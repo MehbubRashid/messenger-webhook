fe5718d248ff938965593afa570d99df79756c31
mehbubrashid <mehbub.rabu@gmail.com>
2020-08-11 18:42:38 +0600

    first fix

--- server.js
+++ server.js
@@ -2,56 +2,56 @@
 
 // Imports dependencies and set up http server
 const
-  express = require('express'),
-  bodyParser = require('body-parser'),
-  app = express().use(bodyParser.json()); // creates express http server
+	express = require('express'),
+	bodyParser = require('body-parser'),
+	app = express().use(bodyParser.json()); 
 const axios = require('axios');
 
 
 // Adds support for GET requests to our webhook
 app.get('/webhook', (req, res) => {
 
-  // Your verify token. Should be a random string.
-  let VERIFY_TOKEN = "cartbot";
-    
-  // Parse the query params
-  let mode = req.query['hub.mode'];
-  let token = req.query['hub.verify_token'];
-  let challenge = req.query['hub.challenge'];
-    
-  // Checks if a token and mode is in the query string of the request
-  if (mode && token) {
-  
-    // Checks the mode and token sent is correct
-    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
-      
-      // Responds with the challenge token from the request
-      console.log('WEBHOOK_VERIFIED');
-      res.status(200).send(challenge);
-    
-    } else {
-      // Responds with '403 Forbidden' if verify tokens do not match
-      res.sendStatus(403);      
-    }
-  }
+	// Your verify token. Should be a random string.
+	let VERIFY_TOKEN = "cartbot";
+
+	// Parse the query params
+	let mode = req.query['hub.mode'];
+	let token = req.query['hub.verify_token'];
+	let challenge = req.query['hub.challenge'];
+
+	// Checks if a token and mode is in the query string of the request
+	if (mode && token) {
+
+		// Checks the mode and token sent is correct
+		if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+
+			// Responds with the challenge token from the request
+			console.log('WEBHOOK_VERIFIED');
+			res.status(200).send(challenge);
+
+		} else {
+			// Responds with '403 Forbidden' if verify tokens do not match
+			res.sendStatus(403);
+		}
+	}
 	else {
-res.send('hahah');
+		res.send('hahah');
 	}
 });
 
 
 // Creates the endpoint for our webhook 
-app.post('/webhook', (req, res) => {  
- 
-  let body = req.body;
-console.log(body);
- axios.post('http://advancedtooltip.000webhostapp.com/wp-admin/admin-post.php', body)
-  .then(function (response) {
-    console.log(response);
-  }); 
-    res.status(200).send('EVENT_RECEIVED');
-  
-
+app.post('/webhook', async (req, res) => {
+	res.status(200).send('EVENT_RECEIVED');
+	let body = req.body;
+	try {
+		// var response = await axios.post('http://advancedtooltip.000webhostapp.com/wp-admin/admin-post.php', body);
+		
+		var response = await axios.post('https://f72ec9758fbd.ngrok.io/woo/wp-admin/admin-post.php', body);
+		console.log(response);
+	} catch (error) {
+		console.log(error);
+	}
 });
 
 
